eda82d1c7ba19862fc899f109806507c02468430
Yury Popov <phoenix@djphoenix.ru>
2013-01-07 01:28:06 +0400

    Updated compilation scripts
    Replaced path to FASM to native link (check your PATH environment
    variable)
    Replaced "echo" strings for more readability

--- compile.bat
+++ compile.bat
@@ -3,7 +3,6 @@ for /f %%i in ("%0") do set curpath=%%~dpi
 cd /d %curpath%
 set CFLAGS=-c -nostdlib -s -m64 -O2 -fno-leading-underscore
 set BIN=bin/pxkrnl
-set FASM="C:\Program Files (x86)\fasm\fasm.exe"
 
 echo === Building kernel ===
 
@@ -15,23 +14,23 @@ call:oc
 goto:eof
 
 :cc
-  echo CC   %~1
+  echo  [CC]	%~1
   x86_64-w64-mingw32-gcc %CFLAGS% src/%~1.cpp -o obj/%~1.o
   set OBJS=%OBJS% obj/%~1.o
 goto:eof
 
 :as
-  echo ASM  %~1
-  %FASM% src/%~1.s obj/%~1.o >nul
+  echo  [ASM]	%~1
+  fasm src/%~1.s obj/%~1.o >nul
   set OBJS=%OBJS% obj/%~1.o
 goto:eof
 
 :link
-  echo LD   %BIN%
+  echo  [LD]	%BIN%
   x86_64-w64-mingw32-ld -belf64-x86-64 -o %BIN% -s --nostdlib %OBJS%
 goto:eof
 
 :oc
-  echo COPY %BIN%
+  echo  [COPY]	%BIN%
   x86_64-w64-mingw32-objcopy %BIN% -Felf32-i386
 goto:eof
--- compile.sh
+++ compile.sh
@@ -3,24 +3,24 @@ CFLAGS="-c -nostdlib -s -m64 -O2"
 BIN="bin/pxkrnl"
 
 function cc {
-  echo "CC   $1"
+  echo " [CC]	$1"
   gcc -m64 $CFLAGS src/$1.cpp -o obj/$1.o > /dev/null
   OBJS="$OBJS obj/$1.o"
 }
 
 function as {
-  echo "ASM  $1"
+  echo " [ASM]	$1"
   fasm src/$1.s obj/$1.o > /dev/null
   OBJS="$OBJS obj/$1.o"
 }
 
 function link {
-  echo "LD   $BIN"
+  echo " [LD]	$BIN"
   ld -o $BIN -s -melf_x86_64 --nostdlib $OBJS > /dev/null
 }
 
 function oc {
-  echo "COPY $BIN"
+  echo " [COPY]	$BIN"
   objcopy $BIN -Felf32-i386
 }
 
