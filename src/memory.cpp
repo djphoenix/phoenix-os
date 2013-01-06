3495af0ebd1f87ea412de195bc45dd078b614ba8
Yury Popov <phoenix@djphoenix.ru>
2013-01-07 01:29:49 +0400

    Memory initialization (uncompleted)
    More messages for memory initialization code

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,5 +1,27 @@
 #include "pxlib.h"
+PGRUB grub_data;
+void l2h(const _int64 i, char* s){
+	for(int c = 0; c < sizeof(i); c++){
+		char b = (i >> ((sizeof(i)-c-1)*8)) & 0xFF;
+		s[c*2+1] = ((b & 0xF) > 9) ? ('A' + (b & 0xF) - 10) : ('0' + (b & 0xF));
+		b >>= 4;
+		s[c*2  ] = ((b & 0xF) > 9) ? ('A' + (b & 0xF) - 10) : ('0' + (b & 0xF));
+	}
+}
 void memory_init()
 {
-  print("Initializing memory...\n");
+	print("Initializing memory...\n");
+	_int64 kernel = 0, stack = 0, kernel_top = 0;
+	kernel = (_int64) grub_data;
+	char* l = "GRUB bios data at '0x0000000000000000'\n";
+	l2h(kernel,&(l[21]));
+	print(l);
+	l = "Kernel loaded into '0x0000000000000000'\n";
+	asm("movq $_start, %q0":"=a"(kernel));
+	l2h(kernel,&(l[22]));
+	print(l);
+	asm("movq $_data_start, %q0":"=a"(kernel));
+	l = "Data section loaded into '0x0000000000000000'\n";
+	l2h(kernel,&(l[28]));
+	print(l);
 }
--- src/memory.h
+++ src/memory.h
@@ -1,5 +1,20 @@
 #ifndef MEMORY_H
 #define MEMORY_H
+typedef struct {
+	long flags;
+	long mem_lower, mem_upper;
+	long boot_device;
+	char *cmdline;
+	long mods_count; void *mods_addr;
+	long syms[3];
+	long mmap_length; void *mmap_addr;
+	long drives_length; void *drives_addr;
+	void *config_table;
+	char *boot_loader_name;
+	void *apm_table;
+	void *vbe_control_info, *vbe_mode_info, *vbe_mode, *vbe_interface_seg, *vbe_interface_off, *vbe_interface_len;
+} GRUB, *PGRUB;
+extern PGRUB grub_data;
 extern void memory_init();
 #include "pxlib.h"
 #endif
