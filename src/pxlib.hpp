e42324324ce1da789c7f6325c78d432fea8cc1e9
Yury Popov <github@phoenix.dj>
2016-05-05 16:50:30 +0300

    Mark pxlib as [extern "C"]

--- src/pxlib.hpp
+++ src/pxlib.hpp
@@ -33,20 +33,22 @@ public:
 typedef unsigned int uint;
 typedef long long int _int64;
 typedef unsigned long long int _uint64;
-extern void print(const char*);
-extern void printb(char i);
-extern void prints(short i);
-extern void printl(int i);
-extern void printq(_int64 i);
-extern void clrscr();
-extern _uint64 strlen(char*,_uint64 limit=-1);
-extern char* strcpy(char*);
-extern bool strcmp(const char*,char*);
-char __inline inportb(short port){ char c; asm("inb %w1, %b0":"=a"(c):"d"(port)); return c; }
-short __inline inports(short port){ short c; asm("inw %w1, %w0":"=a"(c):"d"(port)); return c; }
-uint __inline inportl(short port){ uint c; asm("inl %w1, %d0":"=a"(c):"d"(port)); return c; }
-void __inline outportb(short port, char c){ asm("outb %b0, %w1"::"a"(c),"d"(port)); }
-void __inline outports(short port, short c){ asm("outw %w0, %w1"::"a"(c),"d"(port)); }
-void __inline outportl(short port, uint c){ asm("outl %d0, %w1"::"a"(c),"d"(port)); }
-_uint64 __inline rdtsc() { unsigned long eax, edx; asm("rdtsc":"=a"(eax),"=d"(edx)); _uint64 ret; ret = edx; ret <<= 32; ret |= eax; return ret; }
+extern "C" {
+	extern void print(const char*);
+	extern void printb(char i);
+	extern void prints(short i);
+	extern void printl(int i);
+	extern void printq(_int64 i);
+	extern void clrscr();
+	extern _uint64 strlen(char*,_uint64 limit=-1);
+	extern char* strcpy(char*);
+	extern bool strcmp(const char*,char*);
+	char __inline inportb(short port){ char c; asm("inb %w1, %b0":"=a"(c):"d"(port)); return c; }
+	short __inline inports(short port){ short c; asm("inw %w1, %w0":"=a"(c):"d"(port)); return c; }
+	uint __inline inportl(short port){ uint c; asm("inl %w1, %d0":"=a"(c):"d"(port)); return c; }
+	void __inline outportb(short port, char c){ asm("outb %b0, %w1"::"a"(c),"d"(port)); }
+	void __inline outports(short port, short c){ asm("outw %w0, %w1"::"a"(c),"d"(port)); }
+	void __inline outportl(short port, uint c){ asm("outl %d0, %w1"::"a"(c),"d"(port)); }
+	_uint64 __inline rdtsc() { unsigned long eax, edx; asm("rdtsc":"=a"(eax),"=d"(edx)); _uint64 ret; ret = edx; ret <<= 32; ret |= eax; return ret; }
+}
 #endif
