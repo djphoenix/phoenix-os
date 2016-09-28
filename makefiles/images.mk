acaa02d7de427fc2f03d31db561638f3439439d5
Yury Popov <github@phoenix.dj>
2016-09-28 16:12:56 +0300

    ISO creation refactoring

--- makefiles/images.mk
+++ makefiles/images.mk
@@ -3,15 +3,18 @@ bin/phoenixos: $(BIN)
 	@ echo CP $@
 	@ cp $^ $@
 
-bin/phoenixos.iso: $(BIN) deps/syslinux-$(DEP_SYSLINUX_VER).zip
-	@ mkdir -p $(dir $@)
+.PHONY: isofiles
+
+isofiles: $(BIN) deps/syslinux-$(DEP_SYSLINUX_VER).zip
 	@ mkdir -p $(ISOROOT)
 	@ unzip -q -u -j deps/syslinux-$(DEP_SYSLINUX_VER).zip -d $(ISOROOT) bios/core/isolinux.bin bios/com32/elflink/ldlinux/ldlinux.c32 bios/com32/lib/libcom32.c32 bios/com32/mboot/mboot.c32
 	@ cp $(BIN) $(ISOROOT)/phoenixos
 	@ echo 'default /mboot.c32 /phoenixos' > $(ISOROOT)/isolinux.cfg
+
+bin/phoenixos.iso: isofiles
+	@ mkdir -p $(dir $@)
 	@ echo ISO $@
 	@ $(MKISOFS) -quiet -r -J -V 'PhoeniX OS' -o $@ -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table $(ISOROOT)/
-	@ rm -rf $(ISOROOT)
 
 .PHONY: images kernel iso
 
