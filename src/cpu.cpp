a885ff4bf5a0becef791399de00c19bda12e40b5
Yury Popov <github@phoenix.dj>
2016-09-28 17:34:56 +0300

    CPU class extension + refactoring

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -17,7 +17,72 @@
 #include "cpu.hpp"
 
 char CPU::vendor[13] = "";
+char CPU::brandString[49] = "";
 uint64_t CPU::features = 0;
+uint64_t CPU::features_ext = 0;
+uint64_t CPU::ext_features = 0;
+struct cpu_info CPU::info = { 0xFFFFFFFF, 0, 0 };
+uint32_t CPU::maxCPUID = 0;
+
+static const char CPUID_FEAT_STR[64][16] = {
+  /* 00-03 */"fpu", "vme", "de", "pse",
+  /* 04-07 */"tsc", "msr", "pae", "mce",
+  /* 08-11 */"cx8", "apic", "", "sep",
+  /* 12-15 */"mtrr", "pge", "mca", "cmov",
+  /* 16-19 */"pat", "pse36", "psn", "clfsh",
+  /* 20-23 */"", "ds", "acpi", "mmx",
+  /* 24-27 */"fxsr", "sse", "sse2", "ss",
+  /* 28-31 */"htt", "tm", "ia64", "pbe",
+
+  /* 32-35 */"sse3", "pclmul", "dtes64", "monitor",
+  /* 36-39 */"ds-cpl", "vmx", "smx", "est",
+  /* 40-43 */"tm2", "ssse3", "cnxt-id", "sdbg",
+  /* 44-47 */"fma", "cx16", "xtpr", "pdcm",
+  /* 48-51 */"", "pcid", "dca", "sse4.1",
+  /* 52-55 */"sse4.2", "x2apic", "movbe", "popcnt",
+  /* 56-59 */"tsc-dl", "aes", "xsave", "osxsave",
+  /* 60-63 */"avx", "f16c", "rdrnd", "hv"
+};
+
+static const char CPUID_FEAT_EXT_STR[64][16] = {
+  /* 00-03 */"fsgsbase", "ia32tscadj", "sgx", "bmi1",
+  /* 04-07 */"hle", "avx2", "", "smep",
+  /* 08-11 */"bmi2", "erms", "invpcid", "rtm",
+  /* 12-15 */"pqm", "fpucsds", "mpx", "pqe",
+  /* 16-19 */"avx512f", "avx512dq", "rdseed", "adx",
+  /* 20-23 */"smap", "avx512ifma", "pcommit", "clflushopt",
+  /* 24-27 */"clwb", "intel_pt", "avx512pf", "avx512er",
+  /* 28-31 */"avx512cd", "sha", "avx512bw", "avx512vl",
+
+  /* 32-35 */"prefetchwt1", "avx512vbmi", "", "pku",
+  /* 36-39 */"ospke", "", "", "",
+  /* 40-43 */"", "", "", "",
+  /* 44-47 */"", "", "", "",
+  /* 48-51 */"", "", "", "",
+  /* 52-55 */"", "", "", "",
+  /* 56-59 */"", "", "", "",
+  /* 60-63 */"", "", "", "",
+};
+
+static const char CPUID_EXT_FEAT_STR[64][16] = {
+  /* 00-03 */"fpu", "vme", "de", "pse",
+  /* 04-07 */"tsc", "msr", "pae", "mce",
+  /* 08-11 */"cx8", "apic", "", "syscall",
+  /* 12-15 */"mtrr", "pge", "mca", "cmov",
+  /* 16-19 */"pat", "pse36", "", "mp",
+  /* 20-23 */"nx", "", "mmxext", "mmx",
+  /* 24-27 */"fxsr", "fxsr_opt", "pdpe1gb", "rdtscp",
+  /* 28-31 */"", "lm", "3dnowext", "3dnow",
+
+  /* 32-35 */"lahf_lm", "cmp_legacy", "svm", "extapic",
+  /* 36-39 */"cr8_legacy", "abm", "sse4a", "misalignsse",
+  /* 40-43 */"3dnowprefetch", "osvw", "ibs", "xop",
+  /* 44-47 */"skinit", "wdt", "", "lwp",
+  /* 48-51 */"fma4", "tce", "", "nodeid_msr",
+  /* 52-55 */"", "tbm", "topoext", "perfctr_core",
+  /* 56-59 */"perfctr_nb", "", "dbx", "perftsc",
+  /* 60-63 */"pcx_l2i", "", "", ""
+};
 
 char* CPU::getVendor() {
   if (vendor[0] == 0) {
@@ -38,261 +103,139 @@ char* CPU::getVendor() {
   return vendor;
 }
 
+uint32_t CPU::getMaxCPUID() {
+  if (maxCPUID == 0) {
+    uint32_t eax = 0x80000000, ebx, ecx, edx;
+    asm volatile("cpuid" :
+        "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
+        "a"(eax));
+    maxCPUID = eax;
+  }
+  return maxCPUID;
+}
+
 uint64_t CPU::getFeatures() {
   if (features == 0) {
     uint32_t eax = 1, ebx, ecx, edx;
     asm volatile("cpuid" :
         "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
         "a"(eax));
-    features = ecx;
-    features <<= 32;
-    features |= edx;
+    features = ((uint64_t)edx << 32) | (uint64_t)ecx;
+    union {
+      struct cpu_info {
+        uint32_t stepping:3;
+        uint32_t model:4;
+        uint32_t family:4;
+        uint32_t type:2;
+        uint32_t model_ext:4;
+        uint32_t family_ext:8;
+      } __attribute__((packed)) attrs;
+      uint32_t eax;
+    } cpuid_info;
+    cpuid_info.eax = eax;
+    info.stepping = cpuid_info.attrs.stepping;
+    info.type = cpuid_info.attrs.type;
+    info.model = cpuid_info.attrs.model;
+    info.family = cpuid_info.attrs.family;
+    if (info.family == 6 || info.family == 15) {
+      info.family += cpuid_info.attrs.family_ext;
+      info.model += cpuid_info.attrs.model_ext << 4;
+    }
   }
   return features;
 }
 
-void __inline cf(char*d, const char*f) {
-  uint64_t p = 0, l = 0;
-  while (d[p] != 0)
-    p++;
-  if (p != 0)
-    d[p++] = ' ';
-  while (f[l] != 0) {
-    d[p + (l)] = f[l];
-    l++;
+uint64_t CPU::getFeaturesExt() {
+  if (features_ext == 0) {
+    uint32_t eax = 7, ebx, ecx = 0, edx;
+    asm volatile("cpuid" :
+        "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
+        "a"(eax), "c"(ecx));
+    features_ext = (uint64_t)ebx | ((uint64_t)ecx << 32);
   }
-  d[p + l] = 0;
+  return features_ext;
+}
+
+uint64_t CPU::getExtFeatures() {
+  if (ext_features == 0 && (getMaxCPUID() >= 0x80000001)) {
+    uint32_t eax = 0x80000001, ebx, ecx, edx;
+    asm volatile("cpuid" :
+        "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
+        "a"(eax));
+    ext_features = (uint64_t)edx | ((uint64_t)ecx << 32);
+  }
+  return ext_features;
+}
+
+struct cpu_info CPU::getInfo() {
+  if (info.type == 0xFFFFFFFF)
+    getFeatures();
+  return info;
+}
+
+static inline uint32_t bitcnt(uint64_t val) {
+  uint32_t res = 0;
+  for (uint32_t b = 0; b < 64; b++)
+    res += (val >> b) & 1;
+  return res;
 }
 
 char* CPU::getFeaturesStr() {
-  char* res;
-  uint64_t f = getFeatures(), l = 0;
+  uint64_t f = getFeatures(), ef = getExtFeatures(), fe = getFeaturesExt();
 
-  if (f & CPUID_FEAT_SSE3)
-    l += 5;
-  if (f & CPUID_FEAT_PCLMUL)
-    l += 7;
-  if (f & CPUID_FEAT_DTES64)
-    l += 7;
-  if (f & CPUID_FEAT_MONITOR)
-    l += 8;
-  if (f & CPUID_FEAT_DS_CPL)
-    l += 7;
-  if (f & CPUID_FEAT_VMX)
-    l += 4;
-  if (f & CPUID_FEAT_SMX)
-    l += 4;
-  if (f & CPUID_FEAT_EST)
-    l += 4;
-  if (f & CPUID_FEAT_TM2)
-    l += 4;
-  if (f & CPUID_FEAT_SSSE3)
-    l += 6;
-  if (f & CPUID_FEAT_CID)
-    l += 4;
-  if (f & CPUID_FEAT_FMA)
-    l += 4;
-  if (f & CPUID_FEAT_CX16)
-    l += 5;
-  if (f & CPUID_FEAT_ETPRD)
-    l += 6;
-  if (f & CPUID_FEAT_PDCM)
-    l += 5;
-  if (f & CPUID_FEAT_DCA)
-    l += 4;
-  if (f & CPUID_FEAT_SSE4_1)
-    l += 7;
-  if (f & CPUID_FEAT_SSE4_2)
-    l += 7;
-  if (f & CPUID_FEAT_x2APIC)
-    l += 7;
-  if (f & CPUID_FEAT_MOVBE)
-    l += 6;
-  if (f & CPUID_FEAT_POPCNT)
-    l += 7;
-  if (f & CPUID_FEAT_AES)
-    l += 4;
-  if (f & CPUID_FEAT_XSAVE)
-    l += 6;
-  if (f & CPUID_FEAT_OSXSAVE)
-    l += 8;
-  if (f & CPUID_FEAT_AVX)
-    l += 4;
+  uint32_t count = bitcnt(f) + bitcnt(ef) + bitcnt(fe);
 
-  if ((f >> 32) & CPUID_FEAT_FPU)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_VME)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_DE)
-    l += 3;
-  if ((f >> 32) & CPUID_FEAT_PSE)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_TSC)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_MSR)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_PAE)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_MCE)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_CX8)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_APIC)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_SEP)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_MTRR)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_PGE)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_MCA)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_CMOV)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_PAT)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_PSE36)
-    l += 6;
-  if ((f >> 32) & CPUID_FEAT_PSN)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_CLF)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_DTES)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_ACPI)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_MMX)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_FXSR)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_SSE)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_SSE2)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_SS)
-    l += 3;
-  if ((f >> 32) & CPUID_FEAT_HTT)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_TM1)
-    l += 4;
-  if ((f >> 32) & CPUID_FEAT_IA64)
-    l += 5;
-  if ((f >> 32) & CPUID_FEAT_PBE)
-    l += 4;
+  char buf[count * 17];
+  char *end = &buf[0];
 
-  res = (char*)Memory::alloc(l);
+  for (int i = 0; i < 64; i++) {
+    if (((f & (1 << i)) != 0) && CPUID_FEAT_STR[i][0]) {
+      end += snprintf(end, sizeof(buf), "%s ", CPUID_FEAT_STR[i]);
+    }
+  }
 
-  if (f & CPUID_FEAT_SSE3)
-    cf(res, "sse3");
-  if (f & CPUID_FEAT_PCLMUL)
-    cf(res, "pclmul");
-  if (f & CPUID_FEAT_DTES64)
-    cf(res, "dtes64");
-  if (f & CPUID_FEAT_MONITOR)
-    cf(res, "monitor");
-  if (f & CPUID_FEAT_DS_CPL)
-    cf(res, "ds_cpl");
-  if (f & CPUID_FEAT_VMX)
-    cf(res, "vmx");
-  if (f & CPUID_FEAT_SMX)
-    cf(res, "smx");
-  if (f & CPUID_FEAT_EST)
-    cf(res, "est");
-  if (f & CPUID_FEAT_TM2)
-    cf(res, "tm2");
-  if (f & CPUID_FEAT_SSSE3)
-    cf(res, "ssse3");
-  if (f & CPUID_FEAT_CID)
-    cf(res, "cid");
-  if (f & CPUID_FEAT_FMA)
-    cf(res, "fma");
-  if (f & CPUID_FEAT_CX16)
-    cf(res, "cx16");
-  if (f & CPUID_FEAT_ETPRD)
-    cf(res, "etprd");
-  if (f & CPUID_FEAT_PDCM)
-    cf(res, "pdcm");
-  if (f & CPUID_FEAT_DCA)
-    cf(res, "dca");
-  if (f & CPUID_FEAT_SSE4_1)
-    cf(res, "sse4_1");
-  if (f & CPUID_FEAT_SSE4_2)
-    cf(res, "sse4_2");
-  if (f & CPUID_FEAT_x2APIC)
-    cf(res, "x2apic");
-  if (f & CPUID_FEAT_MOVBE)
-    cf(res, "movbe");
-  if (f & CPUID_FEAT_POPCNT)
-    cf(res, "popcnt");
-  if (f & CPUID_FEAT_AES)
-    cf(res, "aes");
-  if (f & CPUID_FEAT_XSAVE)
-    cf(res, "xsave");
-  if (f & CPUID_FEAT_OSXSAVE)
-    cf(res, "osxsave");
-  if (f & CPUID_FEAT_AVX)
-    cf(res, "avx");
+  for (int i = 0; i < 64; i++) {
+    if (((ef & (1 << i)) != 0) && CPUID_EXT_FEAT_STR[i][0]) {
+      end += snprintf(end, sizeof(buf), "%s ", CPUID_EXT_FEAT_STR[i]);
+    }
+  }
 
-  if ((f >> 32) & CPUID_FEAT_FPU)
-    cf(res, "fpu");
-  if ((f >> 32) & CPUID_FEAT_VME)
-    cf(res, "vme");
-  if ((f >> 32) & CPUID_FEAT_DE)
-    cf(res, "de");
-  if ((f >> 32) & CPUID_FEAT_PSE)
-    cf(res, "pse");
-  if ((f >> 32) & CPUID_FEAT_TSC)
-    cf(res, "tsc");
-  if ((f >> 32) & CPUID_FEAT_MSR)
-    cf(res, "msr");
-  if ((f >> 32) & CPUID_FEAT_PAE)
-    cf(res, "pae");
-  if ((f >> 32) & CPUID_FEAT_MCE)
-    cf(res, "mce");
-  if ((f >> 32) & CPUID_FEAT_CX8)
-    cf(res, "cx8");
-  if ((f >> 32) & CPUID_FEAT_APIC)
-    cf(res, "apic");
-  if ((f >> 32) & CPUID_FEAT_SEP)
-    cf(res, "sep");
-  if ((f >> 32) & CPUID_FEAT_MTRR)
-    cf(res, "mtrr");
-  if ((f >> 32) & CPUID_FEAT_PGE)
-    cf(res, "pge");
-  if ((f >> 32) & CPUID_FEAT_MCA)
-    cf(res, "mca");
-  if ((f >> 32) & CPUID_FEAT_CMOV)
-    cf(res, "cmov");
-  if ((f >> 32) & CPUID_FEAT_PAT)
-    cf(res, "pat");
-  if ((f >> 32) & CPUID_FEAT_PSE36)
-    cf(res, "pse36");
-  if ((f >> 32) & CPUID_FEAT_PSN)
-    cf(res, "psn");
-  if ((f >> 32) & CPUID_FEAT_CLF)
-    cf(res, "clf");
-  if ((f >> 32) & CPUID_FEAT_DTES)
-    cf(res, "dtes");
-  if ((f >> 32) & CPUID_FEAT_ACPI)
-    cf(res, "acpi");
-  if ((f >> 32) & CPUID_FEAT_MMX)
-    cf(res, "mmx");
-  if ((f >> 32) & CPUID_FEAT_FXSR)
-    cf(res, "fxsr");
-  if ((f >> 32) & CPUID_FEAT_SSE)
-    cf(res, "sse");
-  if ((f >> 32) & CPUID_FEAT_SSE2)
-    cf(res, "sse2");
-  if ((f >> 32) & CPUID_FEAT_SS)
-    cf(res, "ss");
-  if ((f >> 32) & CPUID_FEAT_HTT)
-    cf(res, "htt");
-  if ((f >> 32) & CPUID_FEAT_TM1)
-    cf(res, "tm1");
-  if ((f >> 32) & CPUID_FEAT_IA64)
-    cf(res, "ia64");
-  if ((f >> 32) & CPUID_FEAT_PBE)
-    cf(res, "pbe");
+  for (int i = 0; i < 64; i++) {
+    if (((fe & (1 << i)) != 0) && CPUID_FEAT_EXT_STR[i][0]) {
+      end += snprintf(end, sizeof(buf), "%s ", CPUID_FEAT_EXT_STR[i]);
+    }
+  }
 
-  return res;
+  if (end > buf)
+    end--;
+  end[0] = 0;
+
+  return strdup(buf);
+}
+
+char* CPU::getBrandString() {
+  if ((brandString[0] == 0) && (getMaxCPUID() >= 0x80000004)) {
+    typedef union {
+      uint32_t i[12];
+      char s[48];
+    } u;
+    u *v = (u*)brandString;
+    uint32_t eax = 0x80000002, ebx, ecx, edx;
+    asm volatile("cpuid" :
+        "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
+        "a"(eax));
+    v->i[0] = eax, v->i[1] = ebx, v->i[2] = ecx, v->i[3] = edx;
+    eax = 0x80000003;
+    asm volatile("cpuid" :
+        "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
+        "a"(eax));
+    v->i[4] = eax, v->i[5] = ebx, v->i[6] = ecx, v->i[7] = edx;
+    eax = 0x80000004;
+    asm volatile("cpuid" :
+        "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) :
+        "a"(eax));
+    v->i[8] = eax, v->i[9] = ebx, v->i[10] = ecx, v->i[11] = edx;
+    brandString[48] = 0;
+  }
+  return brandString;
 }
