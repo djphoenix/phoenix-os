2a7e5bf18a99d3d9b6a25c9fc3530eae8da212df
Yury Popov <github@phoenix.dj>
2016-09-28 16:18:08 +0300

    EFI launch with bound directory

--- makefiles/utils.mk
+++ makefiles/utils.mk
@@ -1,4 +1,6 @@
 EFIBIOS=$(ODIR)/bios/OVMF.fd
+EFIROOT=$(ODIR)/efi
+EFIKERN=$(EFIROOT)/efi/boot/bootx64.efi
 
 .PHONY: clean check launch launch-efi
 
@@ -15,8 +17,12 @@ $(EFIBIOS): deps/ovmf-$(DEP_OVMF_VER).zip
 	@ mkdir -p $(dir $@)
 	@ unzip -p deps/ovmf-r15214.zip OVMF.fd > $@
 
+$(EFIKERN): $(BIN)
+	@ mkdir -p $(dir $@)
+	@ cp $< $@
+
 launch: $(BIN)
 	$(QEMU) -kernel $< -smp cores=2,threads=2 -cpu Nehalem
 
-launch-efi: $(BIN) $(EFIBIOS)
-	$(QEMU) -kernel $< -smp cores=2,threads=2 -cpu Nehalem -bios $(EFIBIOS)
+launch-efi: $(EFIKERN) $(EFIBIOS)
+	$(QEMU) -drive file=fat:$(EFIROOT) -smp cores=2,threads=2 -cpu Nehalem -bios $(EFIBIOS)
