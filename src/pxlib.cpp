9ed30db38c5460aee347476780611fec461bcd13
Yury Popov <github@phoenix.dj>
2016-10-31 20:02:38 +0300

    Printf refactoring

--- src/pxlib.cpp
+++ src/pxlib.cpp
@@ -60,243 +60,357 @@ void puts(const char *str) {
   LeaveCritical(t);
 }
 
-size_t itoa(uint64_t value, char * str, uint8_t base) {
-  char * ptr;
-  char * low;
-  int len = 0;
-  if (base < 2 || base > 36) {
-    *str = '\0';
-    return len;
-  }
-  ptr = str;
-  if (value < 0 && base == 10) {
-    *ptr++ = '-';
-  }
-  low = ptr;
-  do {
-    *ptr++ = "0123456789abcdefghijklmnopqrstuvwxyz"[ABS(value) % base];
-    value /= base;
-    len++;
-  } while (value);
-  *ptr-- = '\0';
-  while (low < ptr) {
-    char tmp = *low;
-    *low++ = *ptr;
-    *ptr-- = tmp;
-  }
-  return len;
-}
-
-#define LONGFLAG     0x00000001
-#define LONGLONGFLAG 0x00000002
-#define HALFFLAG     0x00000004
-#define HALFHALFFLAG 0x00000008
-#define SIZETFLAG    0x00000010
-#define ALTFLAG      0x00000020
-#define CAPSFLAG     0x00000040
-#define SHOWSIGNFLAG 0x00000080
-#define SIGNEDFLAG   0x00000100
-#define LEFTFORMATFLAG 0x00000200
-#define LEADZEROFLAG 0x00000400
-
-static char *longlong_to_string(char *buf, uint64_t n, size_t len,
-                                uint32_t flag) {
+static char *longlong_to_string(char *buf, size_t len, uint64_t n, uint8_t base,
+                                bool fl_signed, bool fl_showsign,
+                                bool fl_caps) {
   int pos = len;
-  int negative = 0;
-  if ((flag & SIGNEDFLAG) && (int64_t)n < 0) {
+  char table_start = fl_caps ? 'A' : 'a';
+  bool negative = 0;
+  if (fl_signed && (int64_t)n < 0) {
     negative = 1;
     n = -n;
   }
   buf[ --pos] = 0;
-  while (n >= 10) {
-    int digit = n % 10;
-    n /= 10;
-    buf[ --pos] = digit + '0';
+  while (n) {
+    uint8_t digit = n % base;
+    n /= base;
+    if (digit < 10) {
+      buf[ --pos] = digit + '0';
+    } else {
+      buf[ --pos] = digit - 10 + table_start;
+    }
   }
-  buf[ --pos] = n + '0';
-  if (negative)
-    buf[ --pos] = '-';
-  else if ((flag & SHOWSIGNFLAG))
-    buf[ --pos] = '+';
-  return &buf[pos];
-}
-static char *longlong_to_hexstring(char *buf, uint64_t u, size_t len,
-                                   uint32_t flag) {
-  int pos = len;
-  static const char hextable[] = "0123456789abcdef";
-  static const char hextable_caps[] = "0123456789ABCDEF";
-  const char *table = (flag & CAPSFLAG) ? hextable_caps : hextable;
-  buf[ --pos] = 0;
-  do {
-    unsigned int digit = u % 16;
-    u /= 16;
-    buf[ --pos] = table[digit];
-  } while (u != 0);
+  if (negative) buf[ --pos] = '-';
+  else if (fl_showsign) buf[ --pos] = '+';
   return &buf[pos];
 }
 
-#define OUTPUT_CHAR(c) do {\
-  char _c = c; \
-  if (_c == 0) goto done; \
-  if (chars_written < size) str[chars_written] = _c; \
-  chars_written++; \
-} while (0)
+size_t itoa(uint64_t value, char * str, uint8_t base) {
+  char buf[64];
+  char *ret = longlong_to_string(buf, sizeof(buf), value, base, 1, 0, 0);
+  int len = strlen(ret);
+  if (str) {
+    Memory::copy(str, ret, len + 1);
+  }
+  return len;
+}
+
+static inline void printf_putc(char *str, size_t *size, size_t *len, char c) {
+  if ((*len) == (size_t)-1) return;
+  (*len)++;
+  if (str == 0) return;
+  if (*size == 0) {
+    *len = -1;
+    return;
+  }
+  (*size)--;
+  str[*len-1] = c;
+}
 
 int vsnprintf(char *str, size_t size, const char *format, va_list ap) {
+  typedef union {
+    struct {
+      struct {
+        uint8_t fl_leftfmt :1;    // %-
+        uint8_t fl_showsign :1;   // %+
+        uint8_t fl_leadspace :1;  // % [space]
+        uint8_t fl_altfmt :1;     // %#
+        uint8_t fl_leadzero :1;   // %0
+      } __attribute__((packed));
+      struct {
+        uint8_t sz_halfhalf :1;   // %hh
+        uint8_t sz_half :1;       // %h
+        uint8_t sz_long :1;       // %l
+        uint8_t sz_longlong :1;   // %ll
+        uint8_t sz_max :1;        // %j
+        uint8_t sz_sizet :1;      // %z
+        uint8_t sz_ptrdiff :1;    // %t
+        uint8_t sz_longdbl :1;    // %L
+      };
+    } __attribute__((packed));
+    struct {
+      uint32_t raw_flags :5;
+      uint32_t raw_size :8;
+    } __attribute__((packed));
+    uint32_t raw;
+  } __attribute__((packed)) flags_t;
+
   char c;
-  unsigned char uc;
-  const char *s;
-  uint64_t n;
-  void *ptr;
-  int flags;
-  unsigned int format_num;
-  size_t chars_written = 0;
-  char num_buffer[32];
+  const char *fmt_start;
+  size_t out_len = 0;
+
+  flags_t flags;
+  int width, prec;
+  uintmax_t numval;
+  char numbuf[64];
+  const char *strval;
+
+  uint8_t numbase;
+  bool upcaseout, numsig;
+
   for (;;) {
     while ((c = *format++) != 0) {
-      if (c == '%')
+      if (c == '%') {
+        fmt_start = format;
         break;
-      OUTPUT_CHAR(c);
+      }
+      printf_putc(str, &size, &out_len, c);
     }
-    if (c == 0)
-      break;
-    flags = 0;
-    format_num = 0;
-    next_format: c = *format++;
-    if (c == 0)
-      break;
+    if (c == 0) break;
 
-    switch (c) {
-      case '0' ... '9':
-        if (c == '0' && format_num == 0)
-          flags |= LEADZEROFLAG;
-        format_num *= 10;
-        format_num += c - '0';
-        goto next_format;
-      case '.':
-        goto next_format;
-      case '%':
-        OUTPUT_CHAR('%');
-        break;
-      case 'c':
-        uc = va_arg(ap, unsigned int);
-        OUTPUT_CHAR(uc);
-        break;
-      case 's':
-        s = va_arg(ap, const char *);
-        if (s == 0)
-          s = "<null>";
-        goto _output_string;
+    flags.raw = 0;
+    width = 0; prec = 0;
+    numbase = 10;
+    upcaseout = 0;
+
+parse_flags:
+    switch (c = *format++) {  // Flags
       case '-':
-        flags |= LEFTFORMATFLAG;
-        goto next_format;
+        flags.fl_leftfmt = 1;
+        goto parse_flags;
       case '+':
-        flags |= SHOWSIGNFLAG;
-        goto next_format;
+        flags.fl_showsign = 1;
+        goto parse_flags;
+      case ' ':
+        flags.fl_leadspace = 1;
+        goto parse_flags;
       case '#':
-        flags |= ALTFLAG;
-        goto next_format;
-      case 'l':
-        if (flags & LONGFLAG)
-          flags |= LONGLONGFLAG;
-        flags |= LONGFLAG;
-        goto next_format;
-      case 'h':
-        if (flags & HALFFLAG)
-          flags |= HALFHALFFLAG;
-        flags |= HALFFLAG;
-        goto next_format;
-      case 'z':
-        flags |= SIZETFLAG;
-        goto next_format;
-      case 'D':
-        flags |= LONGFLAG;
-        /* @suppress("No break at end of case") */
-      case 'i':
-      case 'd':
-        n = (flags & LONGLONGFLAG) ? va_arg(ap, int64_t) :
-            (flags & LONGFLAG) ? va_arg(ap, int32_t) :
-            (flags & HALFHALFFLAG) ? (int8_t)va_arg(ap, int32_t) :
-            (flags & HALFFLAG) ? (int16_t)va_arg(ap, int32_t) :
-            (flags & SIZETFLAG) ? va_arg(ap, size_t) : va_arg(ap, int32_t);
-        flags |= SIGNEDFLAG;
-        s = longlong_to_string(num_buffer, n, sizeof(num_buffer), flags);
-        goto _output_string;
-      case 'U':
-        flags |= LONGFLAG;
-        /* @suppress("No break at end of case") */
-      case 'u':
-        n = (flags & LONGLONGFLAG) ? va_arg(ap, uint64_t) :
-            (flags & LONGFLAG) ? va_arg(ap, uint32_t) :
-            (flags & HALFHALFFLAG) ? (uint8_t)va_arg(ap, uint32_t) :
-            (flags & HALFFLAG) ? (uint16_t)va_arg(ap, uint32_t) :
-            (flags & SIZETFLAG) ? va_arg(ap, size_t) : va_arg(ap, uint32_t);
-        s = longlong_to_string(num_buffer, n, sizeof(num_buffer), flags);
-        goto _output_string;
-      case 'p':
-        flags |= LONGFLAG | ALTFLAG;
-        goto hex;
-      case 'X':
-        flags |= CAPSFLAG;
-        /* @suppress("No break at end of case") */
-        hex: case 'x':
-        n = (flags & LONGLONGFLAG) ? va_arg(ap, uint64_t) :
-            (flags & LONGFLAG) ? va_arg(ap, uint32_t) :
-            (flags & HALFHALFFLAG) ? (uint8_t)va_arg(ap, uint32_t) :
-            (flags & HALFFLAG) ? (uint16_t)va_arg(ap, uint32_t) :
-            (flags & SIZETFLAG) ? va_arg(ap, size_t) : va_arg(ap, uint32_t);
-        s = longlong_to_hexstring(num_buffer, n, sizeof(num_buffer), flags);
-        if (flags & ALTFLAG) {
-          OUTPUT_CHAR('0');
-          OUTPUT_CHAR((flags & CAPSFLAG) ? 'X': 'x');
-        }
-        goto _output_string;
-      case 'n':
-        ptr = va_arg(ap, void *);
-        if (flags & LONGLONGFLAG)
-          *(int64_t *)ptr = chars_written;
-        else if (flags & LONGFLAG)
-          *(int32_t *)ptr = chars_written;
-        else if (flags & HALFHALFFLAG)
-          *(int8_t *)ptr = chars_written;
-        else if (flags & HALFFLAG)
-          *(int16_t *)ptr = chars_written;
-        else if (flags & SIZETFLAG)
-          *(size_t *)ptr = chars_written;
-        else
-          *(int32_t *)ptr = chars_written;
+        flags.fl_altfmt = 1;
+        goto parse_flags;
+      case '0':
+        flags.fl_leadzero = 1;
+        goto parse_flags;
+      case 0: goto invalid_fmt;
+      default:
+        format--;
+        break;
+    }
+
+parse_width:
+    switch (c = *format++) {  // Width
+      case '0': case '1': case '2': case '3': case '4':
+      case '5': case '6': case '7': case '8': case '9':
+        width *= 10;
+        width += c - '0';
+        goto parse_width;
+      case '*':
+        if (width != 0) goto invalid_fmt;
+        width = va_arg(ap, int);
         break;
       default:
-        OUTPUT_CHAR('%');
-        OUTPUT_CHAR(c);
+        format--;
         break;
     }
-    continue;
-    _output_string: if (flags & LEFTFORMATFLAG) {
-      uint32_t count = 0;
-      while (*s != 0) {
-        OUTPUT_CHAR(*s++);
-        count++;
+
+    if (*format == '.') {
+      format++;
+parse_prec:
+      switch (c = *format++) {  // Prec
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+          prec *= 10;
+          prec += c - '0';
+          goto parse_prec;
+        case '*':
+          if (prec != 0) goto invalid_fmt;
+          prec = va_arg(ap, int);
+          break;
+        default:
+          format--;
+          break;
       }
-      for (; format_num > count; format_num--)
-        OUTPUT_CHAR(' ');
-    } else {
-      uint64_t string_len = strlen(s);
-      char outchar = (flags & LEADZEROFLAG) ? '0' : ' ';
-      for (; format_num > string_len; format_num--)
-        OUTPUT_CHAR(outchar);
-      while (*s != 0)
-        OUTPUT_CHAR(*s++);
     }
+
+parse_size:
+    switch (c = *format++) {  // Size
+      case 'h':
+        if (flags.sz_half) {
+          flags.sz_half = 0;
+          if (flags.raw_size != 0) goto invalid_fmt;
+          flags.sz_halfhalf = 1;
+        } else {
+          if (flags.raw_size != 0) goto invalid_fmt;
+          flags.sz_half = 1;
+        }
+        goto parse_size;
+      case 'l':
+        if (flags.sz_long) {
+          flags.sz_long = 0;
+          if (flags.raw_size != 0) goto invalid_fmt;
+          flags.sz_longlong = 1;
+        } else {
+          if (flags.raw_size != 0) goto invalid_fmt;
+          flags.sz_long = 1;
+        }
+        goto parse_size;
+      case 'j':
+        if (flags.raw_size != 0) goto invalid_fmt;
+        flags.sz_max = 1;
+        goto parse_size;
+      case 'z':
+        if (flags.raw_size != 0) goto invalid_fmt;
+        flags.sz_sizet = 1;
+        goto parse_size;
+      case 't':
+        if (flags.raw_size != 0) goto invalid_fmt;
+        flags.sz_ptrdiff = 1;
+        goto parse_size;
+      case 'L':
+        if (flags.raw_size != 0) goto invalid_fmt;
+        flags.sz_longdbl = 1;
+        goto parse_size;
+      default:
+        format--;
+        break;
+    }
+
+    switch (c = *format++) {
+      case '%':
+        if (flags.raw != 0) goto invalid_fmt;
+        printf_putc(str, &size, &out_len, '%');
+        goto next_format;
+      case 'c':
+        c = va_arg(ap, /* char */ int) & 0xFF;
+        numbuf[0] = c;
+        numbuf[1] = 0;
+        strval = numbuf;
+        goto out_str;
+      case 's':
+        strval = va_arg(ap, const char*);
+        if (strval == 0) strval = "<null>";
+        goto out_str;
+      case 'd':
+      case 'i':
+        numsig = 1;
+        if (flags.sz_halfhalf)
+          numval = va_arg(ap, /* signed char, int8_t */ int32_t);
+        else if (flags.sz_half)
+          numval = va_arg(ap, /* short int, int16_t */ int32_t);
+        else if (flags.sz_long)
+          numval = va_arg(ap, int32_t);
+        else if (flags.sz_longlong)
+          numval = va_arg(ap, int64_t);
+        else if (flags.sz_max)
+          numval = va_arg(ap, intmax_t);
+        else if (flags.sz_sizet)
+          numval = va_arg(ap, size_t);
+        else if (flags.sz_ptrdiff)
+          numval = va_arg(ap, ptrdiff_t);
+        else if (flags.sz_longdbl)
+          goto invalid_fmt;
+        else
+          numval = va_arg(ap, int);
+        goto out_num;
+      case 'X':
+        upcaseout = 1;
+        numbase = 16;
+        goto get_num;
+      case 'p':
+        flags.fl_altfmt = 1;
+        flags.raw_size = 0;
+        flags.sz_longlong = 1;
+        /* @suppress("No break at end of case") */
+      case 'x':
+        numbase = 16;
+        goto get_num;
+      case 'o':
+        numbase = 8;
+        /* @suppress("No break at end of case") */
+      case 'u':
+get_num:
+        numsig = 0;
+        if (flags.sz_halfhalf)
+          numval = va_arg(ap, /* unsigned char, uint8_t */ uint32_t);
+        else if (flags.sz_half)
+          numval = va_arg(ap, /* unsigned short int, uint16_t */ uint32_t);
+        else if (flags.sz_long)
+          numval = va_arg(ap, /* unsigned long int */ uint32_t);
+        else if (flags.sz_longlong)
+          numval = va_arg(ap, /* unsigned long long int*/ uint64_t);
+        else if (flags.sz_max)
+          numval = va_arg(ap, uintmax_t);
+        else if (flags.sz_sizet)
+          numval = va_arg(ap, size_t);
+        else if (flags.sz_ptrdiff)
+          numval = va_arg(ap, ptrdiff_t);
+        else if (flags.sz_longdbl)
+          goto invalid_fmt;
+        else
+          numval = va_arg(ap, int);
+        goto out_num;
+      case 'n': {
+        void *ptrval = va_arg(ap, void*);
+        if (ptrval == 0) goto next_format;
+        if (flags.sz_halfhalf)
+          *((signed char*)ptrval) = out_len;
+        else if (flags.sz_half)
+          *((/* short int */ int16_t*)ptrval) = out_len;
+        else if (flags.sz_long)
+          *((/* long int */ int32_t*)ptrval) = out_len;
+        else if (flags.sz_longlong)
+          *((/* long long int */ int64_t*)ptrval) = out_len;
+        else if (flags.sz_max)
+          *((intmax_t*)ptrval) = out_len;
+        else if (flags.sz_sizet)
+          *((size_t*)ptrval) = out_len;
+        else if (flags.sz_ptrdiff)
+          *((ptrdiff_t*)ptrval) = out_len;
+        else if (flags.sz_longdbl)
+          goto next_format;
+        else
+          *((int*)ptrval) = out_len;
+        goto next_format;
+      }
+      default:
+        format--;
+        break;
+    }
+
+invalid_fmt:
+    format = fmt_start;
+
+next_format:
     continue;
+
+out_num:
+    strval = longlong_to_string(numbuf, sizeof(numbuf),
+                                numval, numbase,
+                                numsig, flags.fl_showsign, upcaseout);
+    if (flags.fl_altfmt) {
+      if (numbase == 8) {
+        printf_putc(str, &size, &out_len, '0');
+        width -= 1;
+      } else if (numbase == 16) {
+        printf_putc(str, &size, &out_len, '0');
+        printf_putc(str, &size, &out_len, upcaseout ? 'X' : 'x');
+        width -= 2;
+      }
+    }
+    goto out_str;
+
+out_str:
+
+    int pad = width - strlen(strval);
+    if (!flags.fl_leftfmt && (flags.fl_leadzero || flags.fl_leadspace)) {
+      c = flags.fl_leadzero ? '0' : ' ';
+      while (pad-- > 0) printf_putc(str, &size, &out_len, c);
+    }
+    while (*strval != 0) printf_putc(str, &size, &out_len, *strval++);
+    if (flags.fl_leftfmt) {
+      while (pad-- > 0) printf_putc(str, &size, &out_len, ' ');
+    }
+    goto next_format;
   }
-  done: return chars_written;
+
+  if (str != 0 && out_len != (size_t)-1) str[out_len] = 0;
+  return out_len;
 }
 
 int vprintf(const char *format, va_list ap) {
   va_list tmp;
   va_copy(tmp, ap);
   int len = vsnprintf(0, 0, format, tmp);
+  va_end(tmp);
   char buf[len + 1];
   len = vsnprintf(buf, len, format, ap);
   buf[len] = 0;
