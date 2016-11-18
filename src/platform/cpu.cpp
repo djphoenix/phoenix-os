ca8f918929041a4d5df3a669cf8ba15973204dd2
Yury Popov <github@phoenix.dj>
2016-11-18 19:34:42 +0300

    Fix eclipse warning about ‘alloca’ in cpu library

--- src/platform/cpu.cpp
+++ src/platform/cpu.cpp
@@ -15,6 +15,7 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "cpu.hpp"
+#include "kernlib.hpp"
 
 char CPU::vendor[13] = "";
 char CPU::brandString[49] = "";
