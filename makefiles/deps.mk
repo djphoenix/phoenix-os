b7c740b29eff2b0521078941908a01f11d8cc519
Yury Popov <github@phoenix.dj>
2017-08-05 06:16:15 +0300

    Update OVMF to 20161202

--- makefiles/deps.mk
+++ makefiles/deps.mk
@@ -12,8 +12,8 @@ DEP_SYSLINUX_VER = 6.03
 DEP_SYSLINUX_URL = https://www.kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.zip
 DEP_SYSLINUX_FILE = syslinux-$(DEP_SYSLINUX_VER).zip
 
-DEP_OVMF_VER = r15214
-DEP_OVMF_URL = http://netcologne.dl.sourceforge.net/project/edk2/OVMF/OVMF-X64-r15214.zip
-DEP_OVMF_FILE = ovmf-$(DEP_OVMF_VER).zip
+DEP_OVMF_VER = 20161202
+DEP_OVMF_URL = http://mirrors.kernel.org/ubuntu/pool/universe/e/edk2/ovmf_0~20161202.7bbe0b3e-1_all.deb
+DEP_OVMF_FILE = ovmf-$(DEP_OVMF_VER).deb
 
 $(foreach d,$(DEP_LIST),$(eval $(call DOWN_RULE,$(d))))
--- makefiles/utils.mk
+++ makefiles/utils.mk
@@ -21,7 +21,7 @@ all: check
 
 $(EFIBIOS): deps/$(DEP_OVMF_FILE)
 	@ mkdir -p $(dir $@)
-	$(Q) unzip -p deps/$(DEP_OVMF_FILE) OVMF.fd > $@
+	$(Q) tar xOf deps/$(DEP_OVMF_FILE) data.tar.xz | tar xO './usr/share/ovmf/OVMF.fd' > $@
 
 $(EFIKERN): $(BIN)
 	@ mkdir -p $(dir $@)
@@ -31,4 +31,4 @@ launch: $(BIN)
 	$(QEMU) -kernel $< -smp cores=2,threads=2 -cpu Broadwell -serial stdio
 
 launch-efi: $(EFIKERN) $(EFIBIOS)
-	$(QEMU) -drive file=fat:$(EFIROOT) -smp cores=2,threads=2 -cpu Broadwell -bios $(EFIBIOS) -serial stdio
+	$(QEMU) -drive file=fat:rw:$(EFIROOT),format=raw,media=disk -smp cores=2,threads=2 -cpu Broadwell -bios $(EFIBIOS) -serial stdio
