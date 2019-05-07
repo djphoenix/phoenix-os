a4e95bba123485c1d245e63f3dc1776b4322f4b0
Yury Popov <github@phoenix.dj>
2019-05-07 11:02:52 +0300

    Lint fixes

--- src/kernlib/include/kernlib/printf.hpp
+++ src/kernlib/include/kernlib/printf.hpp
@@ -4,10 +4,10 @@
 #pragma once
 #include "std.hpp"
 
-int printf(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+int printf(const char *format, ...) __attribute__((format(printf, 1, 2)));
 
 int snprintf(char *str, size_t size, const char *format, ...)
-  __attribute__ ((format (printf, 3, 4)));
+  __attribute__((format(printf, 3, 4)));
 
 int vprintf(const char *format, va_list ap);
 int vsnprintf(char *str, size_t size, const char *format, va_list ap);
