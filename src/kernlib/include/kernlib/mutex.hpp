1575e160f364c3167b9f9dc5a7fa1aa45a5728ec
Yury Popov <github@phoenix.dj>
2019-05-22 16:29:53 +0300

    Mutex refactoring

--- src/kernlib/include/kernlib/mutex.hpp
+++ src/kernlib/include/kernlib/mutex.hpp
@@ -9,6 +9,23 @@ class Mutex {
   bool state;
  public:
   Mutex(): state(0) {}
-  void lock();
-  void release();
+  inline void lock() {
+    asm volatile(
+        "1:"
+        "testw $1, %0; jnz 1b;"
+        "lock btsw $0, %0; jc 1b;"
+        ::"m"(state));
+  }
+  inline bool try_lock() {
+    bool ret = 0;
+    asm volatile(
+        "lock btsw $0, %1; jc 1f;"
+        "mov $1, %0;"
+        "1:"
+        :"=r"(ret):"m"(state));
+    return ret;
+  }
+  inline void release() {
+    asm volatile("movw $0, %0"::"m"(state));
+  }
 };
