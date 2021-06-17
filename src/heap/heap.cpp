fe2a09a7e72416b4210db943fc1878d3eccac9b3
Yury Popov <git@phoenix.dj>
2021-06-17 10:29:48 +0300

    Aligned operator new support

--- src/heap/heap.cpp
+++ src/heap/heap.cpp
@@ -178,10 +178,11 @@ void *Heap::realloc(void *addr, size_t size, size_t align) {
   return newptr;
 }
 
-void* operator new(size_t a) {
-  return Heap::alloc(a);
-}
+void* operator new(size_t a) { return Heap::alloc(a); }
 void* operator new[](size_t a) __attribute__((alias("_Znwm")));
+void* operator new(size_t a, size_t align) __attribute__((alias("_ZN4Heap5allocEmm")));
+void* operator new[](size_t a, size_t align) __attribute__((alias("_Znwmm")));
+
 void operator delete(void* a) noexcept __attribute__((alias("_ZN4Heap4freeEPv")));
 void operator delete[](void* a) noexcept __attribute__((alias("_ZN4Heap4freeEPv")));
 void operator delete(void* a, size_t s) noexcept { Heap::free(a); }
